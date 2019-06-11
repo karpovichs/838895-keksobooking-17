67523d5d0b9a96e107eecd1ab49edd541365c20c
Sonya <sonya.karpovich@gmail.com>
2019-06-11 21:57:05 +0800

    Поправки после проверки

--- js/main.js
+++ js/main.js
@@ -9,47 +9,29 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// Создание массива со случайным порядком чисел от 1 до OFFER_COUNT для аватара
-function createNumberArray() {
-  var number;
-  var used = {};
-  var numArray = [];
-
-  for (var i = 0; i < OFFER_COUNT; i++) {
-    number = getRandomNumber(1, OFFER_COUNT + 1);
-    while (used[number]) {
-      number = getRandomNumber(1, OFFER_COUNT + 1);
-    }
-    used[number] = true;
-    numArray.push(number);
-  }
-  return numArray;
-}
-
 function createOfferArray() {
-  var avatars = createNumberArray();
   var offers = [];
   for (var i = 0; i < OFFER_COUNT; i++) {
-    offers[i] = {
+    offers.push({
       author: {
-        avatar: 'img/avatars/user0' + avatars[i] + '.png'
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
         type: OFFER_TYPES[getRandomNumber(0, OFFER_TYPES.length)],
       },
       location: {
-        x: (getRandomNumber(PIN_WIDTH / 2, mapWidth - PIN_WIDTH) - PIN_WIDTH / 2) + 'px',
-        y: getRandomNumber(130, 631) - PIN_HEIGHT + 'px'
+        x: getRandomNumber(0, mapWidth),
+        y: getRandomNumber(130, 631)
       }
-    };
+    });
   }
   return offers;
 }
 
 function renderPin(pin) {
   var pinElement = pinTemplate.cloneNode(true);
-  pinElement.style.left = pin.location.x;
-  pinElement.style.top = pin.location.y;
+  pinElement.style.left = pin.location.x - PIN_WIDTH / 2 + 'px';
+  pinElement.style.top = pin.location.y - PIN_HEIGHT + 'px';
   pinElement.querySelector('img').src = pin.author.avatar;
   pinElement.querySelector('img').alt = pin.offer.type;
   return pinElement;
